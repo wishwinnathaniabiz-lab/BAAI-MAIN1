d4038066ead454b36951b323f2c877fd3e2dd5a3
Nathania <wishwinnathania.biz@gmail.com>
2025-10-08 16:18:47 +0800

    excel

---
+++ Nathania_Excel.py
@@ -0,0 +1,15 @@
+#
+# Nathania, 2025/09/24
+# File: 0_template.py
+# Short description of the task
+#
+
+import pandas as pd
+
+# 1. Input
+df = pd.read_excel('Financial_Sample.xlsx')
+
+# 2. Process
+
+# 3. Output
+print(df)
