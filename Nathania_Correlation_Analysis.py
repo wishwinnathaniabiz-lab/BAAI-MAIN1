a3c992e12455ac7559e09514b190c5719334897c
Nathania <wishwinnathania.biz@gmail.com>
2025-10-22 16:47:56 +0800

    correlation

--- Nathania_Correlation_Analysis.py
+++ Nathania_Correlation_Analysis.py
@@ -6,23 +6,30 @@
 
 # Data manipulation and analysis
 import pandas as pd
+import numpy as np
 from scipy import stats
 
 # 1. Input
 # Read the CSV file
 df = pd.read_csv('Correlation_Analysis_Data.csv')
 
-correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue']
-)
+df.info()
+# print(df.iloc[:,1:6])
+
+# correlation, pvalue = stats.pearsonr(df['Marketing_Spend'], df['Sales_Revenue']
+# )
 
 # 2. Process
 # print(df.isnull().sum())
 # print(df.isnull().sum().sum())
+# df[].corr()
+correlation_matrix = df.iloc[:,1:6].corr()
+print(correlation_matrix.round(3))
 
 # 3. Output
 # print('Data loaded successfully!')
 # print(f'Dataset shape: {df.shape}')
 # print(f'Correlation: {correlation}')
 # print(f'P value: {pvalue}')
-print(f'Correlation: {correlation:.2f}')
-print(f'P value: {pvalue:.4e}')
+# print(f'Correlation: {correlation:.2f}')
+# print(f'P value: {pvalue:.4e}')
